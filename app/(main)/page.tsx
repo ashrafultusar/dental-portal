f1a29affef8169d234cd0cdc8b2ad7a6f47d226d
ashrafultusar <ashrafulislamtusar@gmail.com>
2026-04-15 10:47:35 +0600

    added all

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,26 +1,25 @@
-import ContactAndAppointment from '@/components/main/appointmentForm/AppointmentForm';
-import Gallery from '@/components/main/home/Gallery/Gallery';
-import Hero from '@/components/main/home/Hero/Hero';
-import Services from '@/components/main/home/Services/Services';
-import Stats from '@/components/main/home/Stats/Stats';
-import VisitingHours from '@/components/main/home/VisitingHours/VisitingHours';
-import OurDoctors from '@/components/main/ourDoctors/OurDoctors';
-import Reviews from '@/components/main/reviews/Reviews';
-
+import ContactAndAppointment from "@/components/main/appointmentForm/AppointmentForm";
+import Gallery from "@/components/main/home/Gallery/Gallery";
+import Hero from "@/components/main/home/Hero/Hero";
+import Services from "@/components/main/home/Services/Services";
+import Stats from "@/components/main/home/Stats/Stats";
+import VisitingHours from "@/components/main/home/VisitingHours/VisitingHours";
+import OurDoctors from "@/components/main/ourDoctors/OurDoctors";
+import Reviews from "@/components/main/reviews/Reviews";
 
 const page = () => {
-    return (
-        <div>
-            <Hero/>
-            <Stats/>
-         <Services/>
-         <OurDoctors/>
-         <Gallery/>
-         <Reviews/>
-         <VisitingHours/>
-         <ContactAndAppointment/>
-        </div>
-    );
+  return (
+    <div>
+      <Hero />
+      <Stats />
+      <Services />
+      <OurDoctors />
+      <Gallery />
+      <Reviews />
+      <VisitingHours />
+      <ContactAndAppointment />
+    </div>
+  );
 };
 
 export default page;
