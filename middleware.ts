9e769d4f613ead2a653e889ff339b340d7f10ec9
ashrafultusar <ashrafulislamtusar@gmail.com>
2026-05-07 00:52:53 +0600

    fix middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,13 @@ export const { auth } = NextAuth(authConfig);
 
 export default auth;
 
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
+// };
+
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)"],
+  matcher: [
+    "/dental-staff-portal/:path*",
+    "/login",
+  ],
 };
